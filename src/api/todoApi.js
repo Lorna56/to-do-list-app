a886107c0d76fbc5ba680cf34d7b15df3077a538
Lorna56 <lornanaula0042@gmail.com>
2025-10-08 16:52:51 +0300

    made slight changes

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://api-to-do-t2zh.onrender.com";
+const API_URL = "https://api-to-do-t2zh.onrender.com/api/todos";
 
 export const getTodos = () => axios.get(API_URL);
 export const createTodo = (data) => axios.post(API_URL, data);
