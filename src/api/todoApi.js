adc189a677905e72919ae99ed1b8fca141e58b80
Lorna56 <lornanaula0042@gmail.com>
2025-10-08 16:05:01 +0300

    Added render link

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/todos";
+const API_URL = "https://api-to-do-t2zh.onrender.com";
 
 export const getTodos = () => axios.get(API_URL);
 export const createTodo = (data) => axios.post(API_URL, data);
